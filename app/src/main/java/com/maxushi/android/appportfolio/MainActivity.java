221d7202594ba65530dbb7fb678d3f2e101768b7
Max <maxxxushi@gmail.com>
2016-12-27 16:39:51 -0500

    code review fixes

--- app/src/main/java/com/maxushi/android/appportfolio/MainActivity.java
+++ app/src/main/java/com/maxushi/android/appportfolio/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,32 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void popularMovies(View view) {
-        toast("This button will launch: popular movies");
-    }
+    public void showToast(View view)
+    {
+        Button button = (Button) view;
+        String appName = button.getText().toString();
 
-    public void stockHawk(View view) {
-        toast("This button will launch: stock hawk");
-    }
-
-    public void buildItBigger(View view) {
-        toast("This button will launch: build it bigger");
-    }
-
-    public void makeYourAppMaterial(View view) {
-        toast("This button will launch: make your app material");
-    }
-
-    public void goUbiquitous(View view) {
-        toast("This button will launch: go ubiquitous");
-    }
-
-    public void capstone(View view) {
-        toast("This button will launch my capstone project");
-    }
-
-    private void toast(String text) {
         Context context = getApplicationContext();
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, appName, Toast.LENGTH_SHORT).show();
     }
 }
